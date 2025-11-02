379c3bde4d6d42220102efe8c661b9619d953224
Khemmie-Ray <atokemmy@gmail.com>
2025-11-02 10:34:58 +0100

    wallet setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,22 @@
-import React from 'react'
+"use client";
+
+import React from "react";
+import { useConnectModal } from "@rainbow-me/rainbowkit";
 
 const Home = () => {
-  return (
-    <div>Home</div>
-  )
-}
+  const { openConnectModal } = useConnectModal();
 
-export default Home
+  return (
+    <>
+      <button
+        onClick={openConnectModal}
+        type="button"
+        className="bg-blue-500 p-4 rounded-xl block my-4"
+      >
+        Open Connect Modal
+      </button>
+    </>
+  );
+};
+
+export default Home;
